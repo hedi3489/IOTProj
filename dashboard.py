7ad5ad2777d3ed8c8330b9da7b22f278cd2df7f7
RetiredHunter <103068609+hedi3489@users.noreply.github.com>
2024-10-31 15:28:29 -0400

    Added Motor setup and functions to turn ON/OFF.
    All code tested and verified.

--- dashboard.py
+++ dashboard.py
@@ -8,13 +8,11 @@ import time
 app = Flask(__name__)
 
 
-#GPIO setup
+# LED setup
 LED_PIN = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
-
-
-#Initial LED state
+# Initial LED state
 led_state = False
 GPIO.output(LED_PIN, GPIO.LOW)
 
@@ -23,6 +21,16 @@ GPIO.output(LED_PIN, GPIO.LOW)
 DHT_PIN = 17
 dht_sensor = DHT(DHT_PIN)
 
+
+# Motor setup
+ENA = 22  # Enable Pin
+IN1 = 27  # Input Pin
+IN2 = 17  # Input Pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(ENA, GPIO.OUT)
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
 
 @app.route('/')
 def dashboard():
@@ -35,18 +43,18 @@ def dashboard():
 def toggle_led():
     #get current LED status
     global led_state
-    
+
     data = request.get_json()
     #parse json data
     switch_state = data['state'] #Get the state value
-    
+
     if switch_state:
         GPIO.output(LED_PIN, GPIO.HIGH) #Turn on LED if switch state is true
         led_state = True #Update led_state to follow that LED is now on
     else:
         GPIO.output(LED_PIN, GPIO.LOW) #Turn off LED if switch state is false
         led_state = False #Update led_state to follow that LED is now off
-        
+
     return jsonify({'success': True, 'led_state': led_state}) # keep this line to indicate that the script passed and the led is turned on or off successfully, otherwise return False and include the current LED state in the response
 
 # Data capture route for reading DHT11 sensor
@@ -57,15 +65,16 @@ def read_sensor():
         if chk == 0:
             humidity = dht_sensor.getHumidity()
             temperature = dht_sensor.getTemperature()
-            
             if temperature > 24:
                 send_email(temperature)
-            
+
             return jsonify({'temperature': temperature, 'humidity': humidity})
         time.sleep(0.1)  # Short delay before retrying
-    
+
     return jsonify({'error': 'Failed to retrieve data from sensor'}), 500
 
+
+# Function to send email
 def send_email(current_temp):
     sender_email = "iotdashboard2024@gmail.com"
     receiver_email = "iotdashboard2024@gmail.com"
@@ -76,7 +85,22 @@ def send_email(current_temp):
     body = f"The current temperature is {current_temp}. Would you like to turn on the fan?"
     yag.send(to=receiver_email, subject=subject, contents=body)
     print("Sent!")
-        
-if __name__ == "__main__":
-    app.run()
 
+
+
+# Function to turn the motor ON
+def turn_motor_on():
+    GPIO.output(ENA, GPIO.HIGH)  # Enable the motor
+    GPIO.output(IN1, GPIO.LOW)   # Set direction
+    GPIO.output(IN2, GPIO.HIGH)  # Set direction
+    print("Motor is ON")
+
+# Function to turn the motor OFF
+def turn_motor_off():
+    GPIO.output(ENA, GPIO.LOW)   # Disable the motor
+    GPIO.output(IN1, GPIO.HIGH)  # Set to stop
+    GPIO.output(IN2, GPIO.LOW)   # Set to stop
+    print("Motor is OFF")
+
+if __name__ == "__main__":
+    app.run(port=5000)
